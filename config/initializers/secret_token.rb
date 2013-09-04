c0c1b4037efdeab33985672b4bc915cfed234711
Thomas Powell <tpowell@appriss.com>
2013-09-04 09:58:36 -0400

    Initial rails new plus cucumber and rspec installs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SightWordFlashCards::Application.config.secret_token = '60adaade87fab2beef5538a6d975157d6f0476285301993a41167637e1720649ec8d36f4f4739a547ceff65c101825f527b029f89d58f6897d329cd85b9011dd'
